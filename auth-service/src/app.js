0969477e31041f157291aa71f32d6ceaadc20481
Divyanshcoder18 <kaliadivyansh77@gmail.com>
2026-05-03 15:10:27 +0530

    makechanges

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 const authRoutes = require('./routes/auth.routes.js');
 
 const app = express();
